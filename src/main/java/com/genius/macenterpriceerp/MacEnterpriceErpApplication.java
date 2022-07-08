fa4cbabbc1c9a8bca08f8c3e4eea222e903fd524
BUSISANI M. DUBE <busisani.dube@nust.ac.zw>
2022-07-08 16:39:02 +0200

    first commit

---
+++ src/main/java/com/genius/macenterpriceerp/MacEnterpriceErpApplication.java
@@ -0,0 +1,13 @@
+package com.genius.macenterpriceerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacEnterpriceErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacEnterpriceErpApplication.class, args);
+    }
+
+}
